a83641fb34e45d0afd1000d5bd7e98fb70d63591
Josel Xavier <115106938+Josel099@users.noreply.github.com>
2023-01-07 19:13:27 +0530

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,10 +4,10 @@ function Notes() {
     <div className="note">
       <h1>note displaying webpage</h1>
       <p1>
-       This is a simple react project. this is my first react project. 
+       This is a simple react project. This is my first react project. 
      </p1>
        
-      <p2>Lorem ipsum olor sit amet, consectetur adipisicing elit. Incidunt magni, fuga quae totam ex quod non sit beatae exercitationem iusto deserunt officia nam ut perspiciatis laboriosam impedit odit nihil architecto!</p2>
+      <p3>Lorem ipsum olor sit amet, consectetur adipisicing elit. Incidunt magni, fuga quae totam ex quod non sit beatae exercitationem iusto deserunt officia nam ut perspiciatis laboriosam impedit odit nihil architecto!</p2>
     </div>
   );
 }
