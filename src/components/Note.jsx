508f083e2fb08f9633afe9c18832fb49cda71e86
Josel Xavier <115106938+Josel099@users.noreply.github.com>
2023-01-07 13:40:54 +0530

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,13 +2,12 @@ import React from "react";
 function Notes() {
   return (
     <div className="note">
-      <h1>This is sample note displaying app</h1>
+      <h1>note displaying webpage</h1>
       <p1>
-       This is a simple react project.which is my first react project. 
-       ////
-
-      </p1>
-      <p2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt magni, fuga quae totam ex quod non sit beatae exercitationem iusto deserunt officia nam ut perspiciatis laboriosam impedit odit nihil architecto!</p2>
+       This is a simple react project. this is my first react project. 
+     </p1>
+       
+      <p2>Lorem ipsum olor sit amet, consectetur adipisicing elit. Incidunt magni, fuga quae totam ex quod non sit beatae exercitationem iusto deserunt officia nam ut perspiciatis laboriosam impedit odit nihil architecto!</p2>
     </div>
   );
 }
