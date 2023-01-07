f440a1f9eaabdd1e4868221020124963f3da32bb
Josel Xavier <115106938+Josel099@users.noreply.github.com>
2023-01-07 13:42:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));  // the whole code is injecting to the div id root
